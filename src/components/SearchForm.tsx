e1efdc9b0b81589f6a8be16642a11b4f19dff63b
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-04-29 10:34:40 -0700

    Widened search button on mobile screens

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -17,7 +17,7 @@ const SearchForm: React.FC<FormComponentProps> = ({ submit, movieRef }) => {
             </input>
             <button className="flex justify-center items-center
             bg-neutral-400 hover:bg-zinc-500 active:bg-neutral-600
-              px-4 py-2 rounded font-black mt-6 md:mt-0"
+              px-4 py-2 rounded font-black mt-6 md:mt-0 max-[767px]:w-full"
             >Search
             </button>
         </form>
