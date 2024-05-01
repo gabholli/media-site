a7134e5ddbd088d145d45209fdaeba7d084c0819
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-05-01 12:24:35 -0700

    Updated titles for each list page

--- src/pages/media/movie_files/Movies.tsx
+++ src/pages/media/movie_files/Movies.tsx
@@ -101,7 +101,7 @@ const Movies = () => {
     <>
       <div className='flex flex-col lg:flex-row justify-between items-center
          py-9 lg:mx-16 gap-y-6 gap-x-8'>
-        <h1 className='text-4xl mt-1 text-center'>Media Site</h1>
+        <h1 className='text-4xl mt-1 text-center'>Media Site - Movies</h1>
         <section className='flex flex-col lg:flex-row px-2 gap-8 '>
           <TopLinks />
           <SearchForm
--- src/pages/media/tvFiles/TV.tsx
+++ src/pages/media/tvFiles/TV.tsx
@@ -103,7 +103,7 @@ const TV = () => {
         <>
             <div className='flex flex-col lg:flex-row justify-between items-center
          py-9 lg:mx-16 gap-y-6 gap-x-8'>
-                <h1 className='text-4xl mt-1 text-center'>Media Site</h1>
+                <h1 className='text-4xl mt-1 text-center'>Media Site - TV</h1>
                 <section className='flex flex-col lg:flex-row px-2 gap-8 '>
                     <TopLinks />
                     <SearchForm
@@ -116,7 +116,7 @@ const TV = () => {
                 </section>
             </div>
             <hr></hr>
-            {tvList.length > 0 ? <h1 className='text-4xl mt-12 text-center'>People</h1> :
+            {tvList.length > 0 ? <h1 className='text-4xl mt-12 text-center'>TV Shows</h1> :
                 <h1 className='text-4xl mt-28 md:mt-48 text-center'>
                     No data currently...
                 </h1>}
